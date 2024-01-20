1688a9194c0343b79cfbd6ff2c1458dbfd24625c
Shahbozzz <mamuraahmedova17@gmail.com>
2024-01-20 10:28:04 +0300

    ILM

--- javascript/header.js
+++ javascript/header.js
@@ -1,4 +1,23 @@
-const menu = document.querySelectorAll('.menu');
-const navbar = document.querySelectorAll('.main-menu');
-menu.addEventListener('click', function(){
-})
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+
+    if (window.scrollY > 10) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
+
+//Loader 
+
+window.addEventListener('DOMContentLoaded', () => {
+    const loader = document.querySelector('.loader');
+    setTimeout(() => {
+        loader.style.opacity='0'
+        setTimeout(()=>{
+            loader.style.display = 'none';
+      },500)
+    }, 1000); 
+  });
+  
